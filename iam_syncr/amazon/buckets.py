dc17b40298306877a49491053bcd644303cedea8
Stephen Moore <stephen@delfick.com>
2015-05-16 17:35:45 +1000

    Add the ability to tag buckets

--- iam_syncr/amazon/buckets.py
+++ iam_syncr/amazon/buckets.py
@@ -2,6 +2,8 @@ from iam_syncr.amazon.documents import AmazonDocuments
 from iam_syncr.amazon.common import AmazonMixin
 from iam_syncr.errors import BadPolicy
 
+from boto.s3.tagging import TagSet, Tags
+from itertools import chain
 import logging
 import boto
 
@@ -31,11 +33,20 @@ class AmazonBuckets(AmazonMixin, object):
                 return "{}"
             raise
 
+    def current_tags(self, bucket):
+        """Return the tags associated with this bucket"""
+        try:
+            return dict(chain.from_iterable([(tag.key, tag.value) for tag in tags] for tags in bucket.get_tags()))
+        except boto.exception.S3ResponseError as error:
+            if error.status == 404:
+                return {}
+            raise
+
     def has_bucket(self, name):
         """Return whether amazon has info about this role"""
         return bool(self.bucket_info(name))
 
-    def create_bucket(self, name, location, permission_document=None):
+    def create_bucket(self, name, location, permission_document=None, tags=None):
         """Create a role"""
         with self.catch_boto_400("Couldn't create bucket", name=name):
             for _ in self.change("+", "bucket[{0}] ".format(location), name=name):
@@ -47,7 +58,7 @@ class AmazonBuckets(AmazonMixin, object):
                 for _ in self.change("+", "bucket_policy", bucket=name, document=permission_document):
                     self.bucket_info(name).set_policy(permission_document)
 
-    def modify_bucket(self, name, location, permission_document):
+    def modify_bucket(self, name, location, permission_document, tags):
         """Modify a bucket"""
         log.info("Inspecting bucket\tname=%s", name)
         bucket = self.bucket_info(name)
@@ -64,4 +75,36 @@ class AmazonBuckets(AmazonMixin, object):
             with self.catch_boto_400("Couldn't modify policy", "Bucket {0} policy".format(name), permission_document, bucket=name):
                 for _ in self.change("M", "bucket_policy", bucket=name, changes=changes):
                     bucket.set_policy(permission_document)
+
+        self.modify_bucket_tags(name, bucket, tags)
+
+    def modify_bucket_tags(self, name, bucket, tags):
+        """Modify the tags on a bucket"""
+        changes = {}
+        new_tags = TagSet()
+        current_tags = self.current_tags(bucket)
+
+        for tag_name, tag_val in tags.items():
+            if tag_name in current_tags:
+                if current_tags[tag_name] != tag_val:
+                    changes[tag_name] = ("modify", tag_name, current_tags[tag_name], tag_val)
+            elif tag_name not in current_tags:
+                changes[tag_name] = ("create", tag_name, None, tag_val)
+
+            new_tags.add_tag(tag_name, tag_val)
+
+        for tag_name in current_tags:
+            if tag_name not in tags:
+                changes[tag_name] = ("delete", tag_name, current_tags[tag_name], None)
+
+        if changes:
+            if not new_tags:
+                for _ in self.change("D", "bucket_tags", bucket=name, changes=["Delete all tags"]):
+                    bucket.delete_tags()
+            else:
+                one_letter = "M" if any(typ in ("modify", "delete") for typ, _, _, _ in changes.values()) else "C"
+                for _ in self.change(one_letter, "bucket_tag", bucket=name, changes=["{0} {1} from {2} to {3}".format(*change) for change in changes.values()]):
+                    t = Tags()
+                    t.add_tag_set(new_tags)
+                    bucket.set_tags(t)
 
--- iam_syncr/buckets.py
+++ iam_syncr/buckets.py
@@ -1,8 +1,10 @@
 from iam_syncr.amazon.buckets import AmazonBuckets
 from iam_syncr.statements import Statements
 from iam_syncr.helpers import listified
+from iam_syncr.errors import SyncrError
 
 import logging
+import six
 
 log = logging.getLogger("iam_syncr.buckets")
 
@@ -19,6 +21,10 @@ class Bucket(object):
 
     def setup(self):
         """Raise errors if the definition doesn't make sense"""
+        self.tags = self.definition.get("tags", {})
+        if type(self.tags) != dict or any(not isinstance(tag_name, six.string_types) or not isinstance(tag_val, six.string_types) for tag_name, tag_val in self.tags.items()):
+            raise SyncrError("Bucket tags should be a dictionary of {<string> : <string>}", got=self.tags)
+
         self.location = self.definition.get("location", "ap-southeast-2")
         for key, default_allow in (("permission", None), ("allow_permission", True), ("deny_permission", False)):
             for policy in listified(self.definition, key):
@@ -31,9 +37,9 @@ class Bucket(object):
 
         bucket_info = self.amazon_buckets.bucket_info(self.name)
         if not bucket_info:
-            self.amazon_buckets.create_bucket(self.name, self.location, permission_document=permission_document)
+            self.amazon_buckets.create_bucket(self.name, self.location, permission_document=permission_document, tags=self.tags)
         else:
-            self.amazon_buckets.modify_bucket(self.name, self.location, permission_document=permission_document)
+            self.amazon_buckets.modify_bucket(self.name, self.location, permission_document=permission_document, tags=self.tags)
 
     def make_permission_document(self, permissions):
         """Return a document for these permissions, or None if no permissiosn"""
